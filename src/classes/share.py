49b69c6e995200bea333f700647d8028cc6b38ce
samwincott <wincott97@gmail.com>
2019-02-20 18:43:37 +0000

    isin added to shares

--- src/classes/share.py
+++ src/classes/share.py
@@ -1,12 +1,14 @@
 class Share(object):
 
-    def __init__(self, ticker, owned, percentage_of_portfolio):
+    def __init__(self, ticker, owned, percentage_of_portfolio, isin):
         self.ticker = ticker
         self.price = self.get_price()
         self.owned = owned
         self.aim_percentage_of_portfolio = percentage_of_portfolio
+        self.isin = isin
     
     def buy(self, amount):
+        assert amount is not None, "Can't sell what you don't have"
         self.owned += amount
     
     def sell(self, amount):
@@ -23,6 +25,9 @@ class Share(object):
     
     def get_price(self):
         return 100.0
+    
+    def get_isin(self):
+        return self.isin
     
     def get_aim_percentage(self):
         return self.aim_percentage_of_portfolio
